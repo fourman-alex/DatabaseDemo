448645026545cd3ba725175d4397755f97a0a463
Alex Fourman <alex@twentyfour.me>
2014-09-28 10:30:45 +0300

    initial

---
+++ DatabaseDemo/Data.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SQLite;
+
+namespace DatabaseDemo
+{
+	public static class Data {
+		static readonly string Path = System.IO.Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "database.db");
+		static SQLite.SQLiteConnection connection;
+		public static SQLite.SQLiteConnection Connection
+		{
+			get {
+				if (connection == null) {
+					connection = new SQLiteConnection (Path,SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex , true);
+				}
+				return connection;
+			}
+		}
+
+
+	}
+
+	public class Valuation
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		[Indexed]
+		public int StockId { get; set; }
+		public DateTime Time { get; set; }
+		public decimal Price { get; set; }
+	}
+	public class Stock
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		[MaxLength(8)]
+		public string Symbol { get; set; }
+
+		public override string ToString ()
+		{
+			return string.Format ("[Stock: Id={0}, Symbol={1}]", Id, Symbol);
+		}
+	}
+
+}
+
---
+++ DatabaseDemo/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Linq;
+
+namespace DatabaseDemo
+{
+	[Activity (Label = "DatabaseDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
+			var textView = FindViewById<TextView> (Resource.Id.textView1);
+
+			var theConn = Data.Connection;
+			theConn.CreateTable<Stock> ();
+			theConn.Insert (new Stock{ Symbol = "MSB" });
+			theConn.Insert (new Stock{ Symbol = "GSG" });
+			theConn.Insert (new Stock{ Symbol = "HWEG" });
+			var table = theConn.Table<Stock> ().ToList ();
+			foreach (var item in table) {
+				textView.Text += item.ToString () + System.Environment.NewLine;
+				Console.WriteLine (item);
+			}
+		}
+	}
+}
+
+
